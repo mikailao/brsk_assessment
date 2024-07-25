597f2255b8e4c8430ea563dfccdc6527abcaad7d
Mikaila Olivier <mikaila@tangentsolutions.co.za>
2024-07-25 22:16:08 +0200

    feat: Added scrabble match function

---
+++ scrabble.py
@@ -0,0 +1,33 @@
+import argparse
+from rich.console import Console
+import random
+
+console = Console()
+
+def find_words(sentence):
+    words = sentence.split()
+    new_words = []
+    for word in words:
+        first_letter = word[0]
+        length = len(word)
+        word_list = []
+        with open('words_alpha.txt', 'r') as file:
+            for line in file:
+                if line.startswith(first_letter) and len(line.strip()) == length:
+                    word_list.append(line.strip())
+        new_word = word_list[random.randint(0, len(word_list))]
+        new_words.append(new_word)
+
+        new_sentence = ' '.join(new_words)
+    console.print(new_sentence)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sentence", help="Sentence to generate words from.")
+    args = parser.parse_args()
+
+    find_words(args.sentence)
+
+if __name__ == '__main__':
+    main()
+
